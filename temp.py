5540670b601283570c9e2213e5a1a1f308cd91d4
ekanboopathi <48629518+ekanboopathi@users.noreply.github.com>
2019-12-13 12:01:18 +0530

    temp

---
+++ temp.py
@@ -0,0 +1,8 @@
+c=int(input("enter the temp:"))
+if c%5==0:
+    x=c/5
+    print(f"It has been convert as fahrenhenit {x*9+32}")
+
+a=int(input("enter the fahrenhenit:"))
+d=a-32
+print(f"It  has been convert as temp {d/9*5}")
